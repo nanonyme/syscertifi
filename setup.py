ac571a682548c2d8015ad9e766b32030d8101661
Seppo Yli-Olli <seppo.yliolli@gmail.com>
2015-04-16 13:58:26 +0300

    Add author info

--- setup.py
+++ setup.py
@@ -2,6 +2,8 @@ from setuptools import setup
 import platform
 
 setup(name='syscertifi',
+      author="Seppo Yli-Olli",
+      author_email="seppo.yli-olli+pypi@iki.fi",
       description="Make requests use system certificate stores",
       py_modules=["certifi"],
       extras_require={":sys_platform=='win32'": ["wincertstore"]},
